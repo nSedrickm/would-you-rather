94a918f32c2154dee104bcd6e9ab8c10a76addc0
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-02 19:43:39 +0100

    update: create loading component

---
+++ src/components/Loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Loader = ({ message }) => {
+    return (
+        <div className="grid h-screen text-white bg-gradient-to-br from-blue-600 to-green-600 place-items-center">
+            <div>
+                <div className="w-16 h-16 mx-auto mb-4 border-b-2 rounded-full border-white-900 animate-spin"></div>
+                <p className="mt-2">{message || "Loading please wait"}</p>
+            </div>
+        </div>
+    );
+}
+
+Loader.propTypes = {
+    message: PropTypes.string
+}
+
