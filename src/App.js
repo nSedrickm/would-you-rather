ac07278e9772a0ed8ee449eb6818f00c9f012de4
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-02 21:41:28 +0100

    refactor: update routing for logged in users only

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Fragment } from "react";
 import { Toaster } from "react-hot-toast";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Login from "./pages/Login";
+import Dashboard from "./pages/DashBoard";
 
 const App = () => {
+  const { authedUser } = useSelector((state) => state.auth);
   return (
     <Fragment>
       <BrowserRouter>
         <Toaster />
-
         <Switch>
+          <Route exact path="/">
+            {authedUser ? <Redirect to="/dashboard" /> : <Redirect to="/login" />}
+          </Route>
           <Route exact path="/login">
-            <Login />
+            {authedUser ? <Redirect to="/dashboard" /> : <Login />}
+          </Route>
+          <Route exact path="/dashboard">
+            {authedUser ? <Dashboard /> : <Redirect to="/login" />}
           </Route>
         </Switch>
-        
+
       </BrowserRouter>
     </Fragment>
   );
