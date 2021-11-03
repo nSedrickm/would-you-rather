55b49f5cc4e0165782e987a9f0d24e0f60c3e3a6
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-03 14:43:49 +0100

    update: create inline loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -16,3 +16,18 @@ Loader.propTypes = {
     message: PropTypes.string
 }
 
+export const InlineLoader = ({ message }) => {
+    return (
+        <div className="grid h-full text-gray-700 bg-white place-items-center">
+            <div>
+                <div className="w-16 h-16 mx-auto mb-4 border-b-2 rounded-full border-white-900 animate-spin"></div>
+                <p className="mt-2">{message || "Loading please wait"}</p>
+            </div>
+        </div>
+    );
+}
+
+InlineLoader.propTypes = {
+    message: PropTypes.string
+}
+
