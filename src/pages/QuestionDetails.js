5ae5d73d0a0e08731414e8163253ee1ce940d559
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-05 22:01:47 +0100

    update: show 404 if poll details not found and redirect to home

--- src/pages/QuestionDetails.js
+++ src/pages/QuestionDetails.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { DetailsCard, Loader } from '../components';
+import NotFound from './NotFound';
 
 
 const QuestionDetails = () => {
     const { qid } = useParams();
-    const history = useHistory();
     const questions = useSelector((state) => state.questions.questions);
     const loading = useSelector((state) => state.questions.loading);
     const question = questions[qid];
@@ -16,25 +16,17 @@ const QuestionDetails = () => {
     return (
         <div className="h-screen p-6 overflow-auto text-gray-700 bg-gradient-to-br from-blue-600 to-green-600 md:p-8">
             <h1 className="mb-10 text-5xl font-bold tracking-wide text-center text-white"> Poll Details </h1>
-
-            <div className="mx-auto rounded-2xl md:w-2/3">
-                {question?.id ? (
+            {question?.id ? (
+                <div className="mx-auto rounded-2xl md:w-2/3">
                     <DetailsCard
                         key={question.id}
                         question={question}
                     />
-                ) : (
-                    <div className="my-10 text-center">
-                        <p className="mb-2 text-2xl text-white">Sorry we couldn't find that question</p>
-                        <button
-                            className="px-12 py-4 my-4 font-bold text-white bg-yellow-500 rounded-lg outline-none"
-                            onClick={() => history.goBack()}
-                        >
-                            back
-                        </button>
-                    </div>
-                )}
-            </div>
+                </div>
+            ) : (
+                <NotFound />
+            )}
+
         </div>
     );
 }
