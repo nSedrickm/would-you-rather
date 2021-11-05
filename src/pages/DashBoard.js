aec84bddb5af9a9ba9c4f50bf62492638e97708e
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-05 05:54:40 +0100

    feat: add option to answer to questions

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -2,48 +2,70 @@ import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { getQuestions } from '../features/questions';
-import { QuestionCard, Loader } from '../components';
+import { QuestionCard, AnsweredQuestionCard, Loader } from '../components';
+
+// helper function to filter questions
+function filterQuestions(questions, answers) {
+    let answered = [];
+    let unanswered = [];
+    Object.keys(questions).forEach((key) => {
+        if (answers[key]) {
+            answered = answered.concat(questions[key])
+        } else {
+            unanswered = unanswered.concat(questions[key])
+        }
+    })
+    return { answered, unanswered };
+};
 
 const DashBoard = () => {
     const [active, setActive] = useState(0);
-    const { users, authedUser } = useSelector((state) => state.auth);
-    const { questions, loading } = useSelector((state) => state.questions);
+    const users = useSelector((state) => state.users.users);
+    const authedUser = useSelector((state) => state.auth.authedUser);
+    const questions = useSelector((state) => state.questions.questions);
+    const loading = useSelector((state) => state.questions.loading);
+    const answers = users[authedUser].answers;
+    const { answered, unanswered } = filterQuestions(questions, answers);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(getQuestions())
-    }, [dispatch])
+        if (!Object.keys(questions).length) {
+            dispatch(getQuestions())
+        }
+    }, [dispatch, questions])
 
     if (loading) return <Loader />
+
     return (
         <div className="h-screen p-6 overflow-auto text-gray-700 bg-gradient-to-br from-blue-600 to-green-600 md:p-8">
-            <h1 className="my-10 text-5xl font-bold tracking-wide text-center text-white"> Dashboard </h1>
+            <h1 className="mb-10 text-5xl font-bold tracking-wide text-center text-white"> Dashboard </h1>
 
             <div className="mx-auto bg-white rounded-2xl md:w-2/3">
                 <div className="flex items-center">
                     <button
-                        className={clsx("flex-grow p-4 text-xl font-bold border-b-2 border-green-500 rounded-tl-2xl outline-none "
+                        className={clsx("flex-grow p-4 text-lg md:text-xl font-bold border-b-2 border-green-500 rounded-tl-2xl outline-none "
                             , active === 0 ? "bg-green-500 text-white" : "text-green-500 bg-white")}
                         onClick={() => setActive(0)}
                     >
-                        Unanswared Polls
+                        Unanswared Questions
                     </button>
                     <button
-                        className={clsx("flex-grow p-4 text-xl font-bold border-b-2 border-green-500 rounded-tr-2xl outline-none "
+                        className={clsx("flex-grow p-4 text-lg md:text-xl font-bold border-b-2 border-green-500 rounded-tr-2xl outline-none "
                             , active === 1 ? "bg-green-500 text-white" : "text-green-500 bg-white")}
                         onClick={() => setActive(1)}
                     >
-                        Answered Polls
+                        Answered Questions
                     </button>
                 </div>
                 {active === 0 ? (
                     <div className="p-4">
-                        {Object.keys(questions)?.map((key) => {
+                        {!unanswered.length && <p>No unanswered questions</p>}
+                        {unanswered?.map(({ id }) => {
                             return (
                                 <QuestionCard
-                                    key={key}
-                                    author={users[questions[key].author]}
-                                    question={questions[key]}
+                                    key={id}
+                                    author={users[questions[id].author]}
+                                    question={questions[id]}
                                     authedUser={authedUser}
                                 />
                             )
@@ -51,7 +73,17 @@ const DashBoard = () => {
                     </div>
                 ) : (
                     <div className="p-4">
-                        answered polls
+                        {!answered.length && <p>No answered questions</p>}
+                        {answered?.map(({ id }) => {
+                            return (
+                                <AnsweredQuestionCard
+                                    key={id}
+                                    author={users[questions[id].author]}
+                                    question={questions[id]}
+                                    answer={answers[id]}
+                                />
+                            )
+                        })}
                     </div>
                 )}
             </div>
